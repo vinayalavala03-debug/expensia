32a13539bec19ad2afb271a0b27c928700dae985
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-05-03 19:32:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,21 @@ const { connectDB } = require('./config/db.js');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigin = 'https://vinay-expense-front.vercel.app';
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

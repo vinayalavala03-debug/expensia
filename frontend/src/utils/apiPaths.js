280cebc53edae3f7efde05a8eb7a75004002d756
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-05-03 14:43:37 +0530

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://expense-tracker-backend-lilac-psi.vercel.app';
+export const BASE_URL = 'https://vinayexpensebackend.vercel.app';
 
 export const API_PATHS = {
   AUTH: {

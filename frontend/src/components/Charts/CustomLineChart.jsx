09150911ec82968a4b21cd304d7b6a37b38f35f7
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-05-05 18:26:13 +0530

    Update CustomLineChart.jsx

--- frontend/src/components/Charts/CustomLineChart.jsx
+++ frontend/src/components/Charts/CustomLineChart.jsx
@@ -9,7 +9,7 @@ const CustomLineChart = ({data}) => {
                 <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
                     <p className="text-xs font-semibold text-purple-800 mb-1">{payload[0].payload.category}</p>
                     <p className="text-sm text-gray-600">
-                        Amount: <span className='text-sm font-medium text-gray-900'>${payload[0].payload.amount}</span></p>
+                        Amount: <span className='text-sm font-medium text-gray-900'>₹{payload[0].payload.amount}</span></p>
                 </div>
             );
         }

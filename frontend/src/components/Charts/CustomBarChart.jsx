308a345b849d8dc7623bf47e95be1c090d31ab83
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-05-05 18:26:47 +0530

    Update CustomBarChart.jsx

--- frontend/src/components/Charts/CustomBarChart.jsx
+++ frontend/src/components/Charts/CustomBarChart.jsx
@@ -23,7 +23,7 @@ const CustomBarChart = ({data}) => {
       return (
         <div className="bg-white shawdow-md rounded-lg p-2 border border-gray-300 ">
           <p className="text-xs font-semibold text-purple-800 mb-1">{payload[0].payload.category}</p>
-          <p className="text-sm text-gray-600">Amount: <span className='text-sm font-medium text-gray-900'>${payload[0].payload.amount}</span></p>
+          <p className="text-sm text-gray-600">Amount: <span className='text-sm font-medium text-gray-900'>₹{payload[0].payload.amount}</span></p>
           <p className="text-sm text-gray-600">Date: <span className='text-sm font-medium text-gray-900'>{payload[0].payload.date}</span></p>
         </div>
       );

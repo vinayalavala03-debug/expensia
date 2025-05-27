42c0c9d9f00253c3cd7cdc8aab60789cd25d93b6
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-05-27 19:56:15 +0530

    Update Navbar.jsx

--- frontend/src/components/Layouts/Navbar.jsx
+++ frontend/src/components/Layouts/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({ activeMenu }) => {
         {openSideMenu ? <HiOutlineX className='text-2xl'/> : <HiOutlineMenu  className='text-2xl'/>}
       </button>
 
-      <h2 className="text-lg font-medium text-black ">Expense Tracker</h2>
+      <h2 className="text-lg font-medium text-black ">Expensia</h2>
 
       {openSideMenu && (
         <div className="fixed top-[61px] -ml-4 bg-white">

89212def20683820837a968148399aad4246daba
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-05-27 20:00:32 +0530

    Update AuthLayout.jsx

--- frontend/src/components/Layouts/AuthLayout.jsx
+++ frontend/src/components/Layouts/AuthLayout.jsx
@@ -5,7 +5,7 @@ import {LuTrendingUpDown } from "react-icons/lu"
 const AuthLayout = ({children}) => {
   return  <div className='flex'>
         <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-            <h2 className='text-black font-medium text-lg'>Expense Tracker</h2>
+            <h2 className='text-black font-medium text-lg'>Expensia - Expense Tracker</h2>
             {children}
         </div>
 

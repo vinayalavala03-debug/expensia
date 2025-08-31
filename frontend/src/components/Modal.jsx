9433cfbcd6cf4e504ed124e257575caa67474a60
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-08-31 20:39:34 +0530

    Update Modal.jsx

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -7,11 +7,11 @@ const Modal = ({children, isOpen, onClose, title}) => {
     <div className='fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50'>
       <div className='relative p-4 w-full max-w-2xl max-h-full'>
         {/** Modal content */}
-        <div className='relative bg-white rounded-lg shadow-sm dark:bg-gray-700'>
+        <div className='relative bg-white rounded-lg shadow-sm dark:bg-white'>
           
           {/* Modal header */}
           <div className='flex items-center justify-between  p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200'>
-            <h3 className='text-lg font-medium text-gray-900 dark:text-white'>
+            <h3 className='text-lg font-medium text-gray-900 dark:text-gray'>
               {title}
             </h3>
 

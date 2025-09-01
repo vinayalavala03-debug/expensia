934a6d5d85a36ae2fd318040a0cc033278eaee2b
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-09-01 18:33:53 +0530

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://expense-tracker-back-eight.vercel.app';
+export const BASE_URL = 'http://localhost:4001';
 
 export const API_PATHS = {
   AUTH: {
@@ -14,6 +14,13 @@ export const API_PATHS = {
     GET_ALL_INCOME: "/api/v1/income/get",
     DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
     DOWNLOAD_INCOME: "/api/v1/income/download"
+  },
+  TRIP: {
+    GET_ALL_TRIPS: "/api/v1/trips",
+    ADD_TRIP: "/api/v1/trips",
+    GET_TRIP_DETAILS: (id) => `/api/v1/trips/${id}`,
+    ADD_EXPENSE_TO_TRIP: (id) => `/api/v1/trips/${id}/expenses`,
+    ADD_INCOME_TO_TRIP: (id) => `/api/v1/trips/${id}/incomes`,
   },
   EXPENSE: {
     ADD_EXPENSE: "/api/v1/expense/add",

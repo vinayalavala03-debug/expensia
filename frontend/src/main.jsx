57787154ccd6fc90ccd8c51a75ae8ccda72e7707
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-09-01 20:02:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import UserProvider from './context/UserContext.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,3 +13,5 @@ createRoot(document.getElementById('root')).render(
     </UserProvider>
   </React.StrictMode>
 )
+
+serviceWorkerRegistration.register();

d072e4f3c5610225e1ac760cbd97d1415c102047
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-09-01 20:01:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Trip Planner App",
+        short_name: "TripApp",
+        theme_color: "#2563eb",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          { src: "icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "icons/icon-512x512.png", sizes: "512x512", type: "image/png" }
+        ]
+      }
+    })
+  ],
 })

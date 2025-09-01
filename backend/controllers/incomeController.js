2c4743b19078cea8bb64af508adec2729e76e6af
Alavala-Vinay <142196470+Alavala-Vinay@users.noreply.github.com>
2025-09-01 08:42:11 +0530

    Update incomeController.js

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -18,7 +18,7 @@ exports.addIncome = async (req, res) => {
             icon,
             source,
             amount,
-            description
+            description,
             date:new Date(date)
         });
 

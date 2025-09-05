c9e589b81adca89c6403b10f6a40f46b4e911647
vinayalavala03-debug <vinayalavala03@gmail.com>
2025-09-05 22:47:10 +0530

    Update FinanceOverview.jsx

--- frontend/src/components/Dashboard/FinanceOverview.jsx
+++ frontend/src/components/Dashboard/FinanceOverview.jsx
@@ -21,7 +21,7 @@ const FinanceOverview = ({ totalBalance, totalIncome, totalExpense }) => {
 
   return <div className='card'>
       <div className='flex items-center justify-center'>
-        <h5 className='text-lg'>Financial Overview</h5>
+        <h5 className='text-lg'> Total Financial Overview</h5>
       </div>
 
       <CustomPieChart

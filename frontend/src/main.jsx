ab42309fcf90a9ccfb487870f359ca012d3e3952
vinayalavala03-debug <vinayalavala03@gmail.com>
2025-09-06 16:33:04 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import UserProvider from './context/UserContext.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
-      <App />
+        <GoogleOAuthProvider clientId="742704150829-jinefqh3ir18595t2srtfa8s35s7uh80.apps.googleusercontent.com">
+          <App />
+        </GoogleOAuthProvider>
     </UserProvider>
   </React.StrictMode>
 )
